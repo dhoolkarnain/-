579eefe532812fa8a4b813f79ac3d87e0c9e0cbd
Sufyan Ahmad Ansari <65646548+dhoolkarnain@users.noreply.github.com>
2022-05-31 21:02:57 +0530

    Add files via upload

---
+++ CommandTest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class CommandTest
+{
+    public static void main(String args[])
+    {
+         for(String s:args)
+         {
+             System.out.println(s);
+         }
+    }
+}
